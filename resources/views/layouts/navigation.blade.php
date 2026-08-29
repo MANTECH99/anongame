54327c1517c771a32128e6574b7679617551c1fb
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2026-08-29 17:17:54 +0000

    first commit 18

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,10 +31,10 @@
                         <x-dropdown-link :href="route('devinette.create')">Créer une devinette</x-dropdown-link>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-                            <x-dropdown-link :href="route('logout')"
-                                onclick="event.preventDefault(); this.closest('form').submit();">
+                            <button type="submit"
+                                class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
                                 Se déconnecter
-                            </x-dropdown-link>
+                            </button>
                         </form>
                     </x-slot>
                 </x-dropdown>
